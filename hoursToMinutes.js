f104c22c9a0ae1b34524b1123cabf4acc1851f6b
Sazib <mahiburislamsazib@gmail.com>
2024-07-04 19:46:41 +0600

    hours to min

---
+++ hoursToMinutes.js
@@ -0,0 +1,11 @@
+// hours to minutes
+
+function HoursToMinutes(hours) {
+    return hours * 60;
+}
+
+const hours = 5;
+const minutes = HoursToMinutes(hours);
+console.log("minutes:", minutes,);
+
+
